c258dd70b1d0b8f4e5a4b42c1acf3711a3970dbc
dschatzl <derek@schatzlein.com>
2022-10-03 09:41:43 -0500

    Add files via upload

---
+++ p07_pizza_cost.py
@@ -0,0 +1,8 @@
+# One pizza costs $12. Ask the user how many pizzas they want with the prompt
+# 'Enter number of pizzas: ' inside of int(input()) (see the previous question
+# if you are confused about how to do this).
+# Multiply the value that the user enters by 12 to get their total price,
+# and store their total in a variable named total_price.
+# Print their total cost to them beginning with 'Your total is $'
+# For example: if the user says they want 3 pizzas, your program should print
+#'Your total is $36'
---
+++ p11_if_else_can_vote_years_left.py
@@ -0,0 +1,12 @@
+# A person can vote if their age is greater than or equal to 18.
+# Ask the user for their age with the prompt 'Enter your age: ' inside
+# int(input()). If the user's age is 18 or more, print 'You can vote'
+
+# Otherwise subtract the users age from 18 and tell them how much longer until
+# they can vote, print 'You can vote in', followed by the years remanining,
+# followed by 'years'
+# You need to use an if statement with an else.
+
+# For example: if a user says their age is 15, your code should show this:
+# Enter your age: 15
+# You can vote in 3 years
