babee80c2c4ad50ad6514050dbd7ac49a9daa377
dschatzl <derek@schatzlein.com>
2022-10-03 10:01:45 -0500

    Add files via upload

---
+++ src/p08_money_made.py
@@ -0,0 +1,14 @@
+# Your user works in sales. The user will tell you how much money they make per
+# item, and how many items they sold. Use this information to calculate, and
+# display the total amount they made by multiplying the two together.
+# Use the prompts 'Enter amount per item: ' and 'Enter number of items sold: '
+# in that order, then display 'You made $' followed by the amount made.
+#
+# An example run of the program with $5 per item and 8 items sold would look
+# like this:
+#
+# Enter amount per item: 5
+# Enter number of items sold: 8
+# You made $40
+
+
